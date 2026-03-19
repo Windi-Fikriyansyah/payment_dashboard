e4ecd69ffd583ee013688e5443853b9fb1246234
windi fikriyansyah <diwin321@gmail.com>
2026-03-19 15:32:03 +0700

    dsdsd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} - Linkbayar</title>
+    <title>{{ config('app.name', 'Laravel') }} - Link Pembayaran</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
